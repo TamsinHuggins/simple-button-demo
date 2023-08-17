e412cf17b31eed69fe1fbd5c732636411c376070
Tamsin Huggins <tamsin.hns@gmail.com>
2023-08-17 14:12:20 +0100

    simple button click demo

---
+++ myscript.js
@@ -0,0 +1,9 @@
+console.log("the script is connected");
+
+function changeDivColor() {
+  // function that that changes the box to red when called.
+  // access the div
+  let myDiv = document.getElementById("myDiv");
+  // change its color
+  myDiv.style.backgroundColor = "red";
+}
